1ca642b14a72a2feb7c0defdfd4e227c6ae8f7c4
Daniel Ghimpu <smeu.valentina33@gmail.com>
2021-06-17 21:28:43 +0100

    finished

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react"
 
 function Footer() {
   return (
-    <div className="footer">Copyright © Movie Catalog</div>
+    <div className="footer">Copyright © Valy</div>
   )
 
 }
--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import image from "../Images/picture-sorry.png"
+import Image from "../Images/picture-sorry.png";
 
 function MainContent() {
   const [search, setSearch] = useState("pride and prejudice");
@@ -15,14 +15,14 @@ function MainContent() {
       .then((response) => response.json())
       .then((data) => {
         setListFilm(data.Search);
-        const temp = Math.ceil(data.totalResults / 10)
-        setNrPages(temp)
+        const temp = Math.ceil(data.totalResults / 10);
+        setNrPages(temp);
         console.log(data);
       });
   }, [currentPage]);
-  const pagesList = []
+  const pagesList = [];
   for (let i = 1; i <= nrPages; i++) {
-    pagesList.push(i)
+    pagesList.push(i);
   }
   function getMovies(event) {
     fetch(
@@ -39,9 +39,7 @@ function MainContent() {
         for (let i = 1; i <= nrPages; i++) {
           pagesList.push(i);
         }
-
       });
-
   }
 
   return (
@@ -52,101 +50,123 @@ function MainContent() {
           type="text "
           placeholder="Search movie..."
         />
-        <BtnSearch onClick={getMovies} >Search</BtnSearch>
+        <BtnSearch onClick={getMovies}>Search</BtnSearch>
       </div>
 
       <div className="box">
+        {!listFilm && <Sorry>Sorry no movie found!</Sorry>}
 
         {listFilm?.map(function (film) {
           return (
             <div className="row" style={{ backgroundColor: "#003566" }}>
-              {!film.Poster &&
-                <img className="col left poster" src={image} alt={film.Title}></img>}
-              <img className="col left poster" src={film.Poster} alt={film.Title}></img>
+              {film.Poster !== "N/A" && (
+                <img
+                  className="col left poster"
+                  src={film.Poster}
+                  alt={film.Title}
+                ></img>
+              )}
+              {film.Poster === "N/A" && (
+                <img
+                  className="col left poster"
+                  src={Image}
+                  alt={film.Title}
+                ></img>
+              )}
               <div className="col right">
-                <p >{film.Title}</p>
-                <p >{film.Year}</p>
+                <p>{film.Title}</p>
+                <p>{film.Year}</p>
               </div>
             </div>
           );
         })}
       </div>
-      <div className="buttons">
-        <button
-          className="btn"
-          style={{ backgroundColor: "#ffd60a", cursor: "pointer" }}
-          onClick={(event) => setCurrentPage(1)}
-          disabled={currentPage === 1}
-        >
-          First
-        </button>
-        <button
-          style={{ backgroundColor: "#ffd60a", cursor: "pointer" }}
-          onClick={(event) => setCurrentPage(Math.abs(currentPage - 1))}
-          disabled={currentPage === 1}
-        >
-          prev
-        </button>
-        <div className="mapBtn">
-          {pagesList.map((page) => {
-            return (
-              <button
-                style={{
-                  backgroundColor: page === currentPage ? "#ffc300" : "#ffd60a",
-                  fontSize: "2rem",
-                  cursor: "pointer",
-                }}
-                onClick={() => setCurrentPage(page)}
+      {listFilm && (
+        <div className="buttons">
+          <BtnEnd
+            className="btn"
 
-              >
-                {page}
-              </button>
-            );
-          })}
+            onClick={(event) => setCurrentPage(1)}
+            disabled={currentPage === 1}
+          >
+            First
+          </BtnEnd>
+          <BtnEnd
+
+            onClick={(event) => setCurrentPage(Math.abs(currentPage - 1))}
+            disabled={currentPage === 1}
+          >
+            prev
+          </BtnEnd>
+          <div className="mapBtn">
+            {pagesList.map((page) => {
+              return (
+                <button
+                  style={{
+                    backgroundColor:
+                      page === currentPage ? "#ffc300" : "#ffd60a",
+                    fontSize: "2rem",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => setCurrentPage(page)}
+                >
+                  {page}
+                </button>
+              );
+            })}
+          </div>
+          <BtnEnd
+
+            onClick={(event) => setCurrentPage(Math.abs(currentPage + 1))}
+            disabled={currentPage === nrPages}
+          >
+            next
+          </BtnEnd>
+          <BtnEnd
+
+            onClick={(event) => setCurrentPage(nrPages)}
+            disabled={currentPage === nrPages}
+          >
+            Last
+          </BtnEnd>
         </div>
-        <Btn
-          style={{ backgroundColor: "#ffd60a", cursor: "pointer" }}
-          onClick={(event) => setCurrentPage(Math.abs(currentPage + 1))}
-          disabled={currentPage === nrPages}
-        >
-          next
-        </Btn>
-        <button
-          style={{ backgroundColor: "#ffd60a", cursor: "pointer" }}
-          onClick={(event) => setCurrentPage(nrPages)}
-          disabled={currentPage === nrPages}
-        >
-          Last
-        </button>
-      </div>
-    </div >
+      )}
+    </div>
   );
 }
 
 export default MainContent;
 
-
 const Btn = styled.button`
-      background-color: #ffc300;
-      cursor: pointer;
-      `;
+  background-color: #ffc300;
+  cursor: pointer;
+`;
 const Field = styled.input`
-      font-size: 1.5rem;
-      border-radius: 2rem;
-      width: 30rem;
-      height: 2rem;
-      border: none;
-
-
-
-      `;
+  font-size: 1.5rem;
+  border-radius: 2rem;
+  width: 30rem;
+  height: 2rem;
+  border: none;
+`;
 const BtnSearch = styled.button`
-      font-size: 1.5rem;
-      background-color: #ffc300;
-      cursor: pointer;
-      border-radius: 2rem;
-      width: 8rem;
-      height: 2.5rem;
-
-
-      `;
+  font-size: 1.5rem;
+  background-color: #ffc300;
+  cursor: pointer;
+  border-radius: 2rem;
+  width: 8rem;
+  height: 2.5rem;
+  &:hover {
+    background: #ffd60a;
+  }
+`;
+const Sorry = styled.div`
+  font-size: 3rem;
+  color: #ffd60a;
+`;
+const BtnEnd = styled.button`
+  background-color: #ffc300;
+  cursor: pointer;
+  &:hover {
+    background: #ffd60a;
+  }
+`;
